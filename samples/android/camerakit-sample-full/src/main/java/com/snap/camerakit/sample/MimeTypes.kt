0e171d99823c58174d80d48c0ccb7396a2c533ef
Pavel Sharanda <psharanda@snapchat.com>
2024-11-07 14:58:09 +0000

    [All] Sync changes for the 1.34.0 release

--- samples/android/camerakit-sample-full/src/main/java/com/snap/camerakit/sample/MimeTypes.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("MimeTypes")
-
-package com.snap.camerakit.sample
-
-internal const val MIME_TYPE_IMAGE_JPEG = "image/jpeg"
-internal const val MIME_TYPE_VIDEO_MP4 = "video/mp4"
-internal const val MIME_TYPE_JSON = "application/json"
